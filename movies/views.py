54e4c279e8555cecdd18988ad2a11692caaad910
Nishi <nishishah010@gmail.com>
2024-08-19 21:45:46 +0530

    task2

--- movies/views.py
+++ movies/views.py
@@ -3,15 +3,29 @@ import requests
 from django.http import HttpResponse,JsonResponse
 from django.shortcuts import render
 from django.conf import settings
+import random
 
 
 def home(request):
     api_key = settings.OMDB_API_KEY
-    fav_movie='Jab We Met'
+    fav_movie='Harry Potter'
     url= f'https://www.omdbapi.com/?s={fav_movie}&apikey={api_key}&page=1'
     response=requests.get(url)
     movies=response.json().get('Search',[])
+    
+
+    #task2
+    genre=request.GET.get('genre')
+    print(genre)
+    if genre:
+        genreUrl= f'https://www.omdbapi.com/?s={genre}&apikey={api_key}&page=1'
+        genreResponse = requests.get(genreUrl)
+
+        genreMovie=genreResponse.json().get('Search',[])
+        movies=[movie for movie in genreMovie if genre.lower() in movie.get('Genre',' ').lower()]
+        print(genreResponse)
     return render(request,'movie.html',{'movies':movies})
+
 
 def details(request,title):
     api_key=settings.OMDB_API_KEY
