0b8db6e1813302d4b88221ca47e8b0cbc95f7ba2
MonilMehta <monilmehta5@gmail.com>
2024-08-21 23:21:51 +0530

    Fixed views.py and html structure

--- movies/urls.py
+++ movies/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('',views.admin,name='home'),
-    path('details/<str:title>/',views.details,name='details'),
+    # path('',views.admin,name='home'),
+    path('details/',views.details,name='details'),
+    path('',views.details,name='details'),
     path('searched/',views.searched,name='searched')
 ]
--- movies/views.py
+++ movies/views.py
@@ -5,29 +5,28 @@ from django.conf import settings
 
 def details(request):
     api_key = settings.OMDB_API_KEY
-    fav_movie = request.GET.get('fav_movie', ' ')
-    url = f'https://www.omdbapi.com/?t={fav_movie}&apikey={api_key}&page=1'
-    response = requests.get(url)
-    movie_data = response.json()
+    fav_movie = request.GET.get('title')
+    if fav_movie:
+        url = f'https://www.omdbapi.com/?t={fav_movie}&apikey={api_key}&page=1'
+        response = requests.get(url)
+        movie_data = response.json()
+        print(movie_data)
 
 
-    movie, created = Moviee.objects.get_or_create(
-        title=movie_data['Title'],
-        defaults={
-            'year': movie_data.get('Year'),
-            'genre': movie_data.get('Genre'),
-            'plot': movie_data.get('Plot'),
-            'poster': movie_data.get('Poster'),
-            
-        }
-    )
-
-    if not created:
-
-        movie.search_count += 1
+        movie, created = Moviee.objects.get_or_create(
+            title=movie_data['Title'],
+            year=movie_data['Year'],
+            genre=movie_data['Genre'],
+            plot=movie_data['Plot'],
+            poster=movie_data['Poster'],
+        )
+        movie.search += 1
         movie.save()
 
-    return render(request, 'movie.html', {'movies': [movie]})
+        return render(request, 'movie.html', {'movies': movie_data})
+
+    return render(request, 'movie.html', {'movies': []})
+
 
 def searched(request):
     movies=Moviee.objects.all()
